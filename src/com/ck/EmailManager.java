b3e1175d0d13c9e689df3045e2c20082a7d28b9a
kongaman <christian.kranz@gmail.com>
2021-04-16 11:04:51 +0200

    get folders from email account

--- src/com/ck/EmailManager.java
+++ src/com/ck/EmailManager.java
@@ -1,5 +1,6 @@
 package com.ck;
 
+import com.ck.controller.services.FetchFoldersService;
 import com.ck.model.EmailAccount;
 import com.ck.model.EmailTreeItem;
 
@@ -15,12 +16,9 @@ public class EmailManager {
     }
 
     public void addEmailAccount(EmailAccount emailAccount){
-        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
-        treeItem.setExpanded(true);
-            treeItem.getChildren().add(new TreeItem<String>("INBOX"));
-            treeItem.getChildren().add(new TreeItem<String>("Sent"));
-            treeItem.getChildren().add(new TreeItem<String>("Folder1"));
-            treeItem.getChildren().add(new TreeItem<String>("Spam"));
+    	EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        fetchFoldersService.start();    
         foldersRoot.getChildren().add(treeItem);
     }
 }
