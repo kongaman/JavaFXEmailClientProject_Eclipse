8c35273e43cdf427fcfe0e02265e7c5bc5005ecc
kongaman <christian.kranz@gmail.com>
2021-05-07 19:10:27 +0200

    made attachments deletable

--- src/com/ck/controller/ComposeMessageController.java
+++ src/com/ck/controller/ComposeMessageController.java
@@ -1,21 +1,30 @@
 package com.ck.controller;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
 
 import com.ck.EmailManager;
 import com.ck.controller.services.EmailSenderService;
 import com.ck.model.EmailAccount;
 import com.ck.view.ViewFactory;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -35,15 +44,30 @@ public class ComposeMessageController extends BaseController implements Initiali
     @FXML
     private ChoiceBox<EmailAccount> emailAccountChoice;
     @FXML
+    private HBox hBoxAttachments;
+    @FXML
     void attachButtonAction() {
     	FileChooser fileChooser = new FileChooser();
     	File selectedFile = fileChooser.showOpenDialog(null);
     	if(selectedFile != null) {
     		attachments.add(selectedFile);
+    		showAttachment(selectedFile);
     	}
     }
 
-    @FXML
+    private void showAttachment(File selectedFile) {
+    	AttachmentButton button;
+		try {
+			button = new AttachmentButton(selectedFile);
+			hBoxAttachments.getChildren().add(button);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+    	
+		
+	}
+
+	@FXML
     void sendButtonAction() {
     	EmailSenderService emailSenderService = new EmailSenderService(emailAccountChoice.getSelectionModel().getSelectedItem(),
     			subjectTextField.getText(), recipientTextField.getText(), htmlEditor.getHtmlText(), attachments);
@@ -75,6 +99,24 @@ public class ComposeMessageController extends BaseController implements Initiali
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		emailAccountChoice.setItems(emailManager.getEmailAccounts());
 		emailAccountChoice.setValue(emailManager.getEmailAccounts().get(0));
+	}
+	
+	private class AttachmentButton extends Button  {
+		
+		private File attachment;
+
+		public AttachmentButton(File attachedFile) throws MessagingException {
+			this.attachment = attachedFile;
+			this.setText("Delete: " + attachedFile.getName());
+			
+			this.setOnAction(event -> deleteAttachment(attachedFile));
+		}
+		
+		private void deleteAttachment(File attachedFile) {
+			attachments.remove(attachedFile);
+			hBoxAttachments.getChildren().remove(this);
+		}
+		
 	}
 
 }
