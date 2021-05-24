013481fc83d9ada34641bc97e71bf151d669370f
kongaman <christian.kranz@gmail.com>
2021-05-24 12:34:47 +0200

    persistence basic methods

--- src/com/ck/controller/persistence/PersistenceAccess.java
+++ src/com/ck/controller/persistence/PersistenceAccess.java
@@ -1,12 +1,40 @@
 package com.ck.controller.persistence;
 
-public class PersistenceAccess {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-	public void loadFromPersistence() {
-		
+public class PersistenceAccess {
+	
+	private String VALID_ACCOUNTS_LOCATION = System.getenv("APPDATA") + "\\validAccount.ser";
+
+	public List<ValidAccount> loadFromPersistence() {
+		List<ValidAccount> resultList = new ArrayList<>();
+		try {
+			FileInputStream fileInputStream = new FileInputStream(VALID_ACCOUNTS_LOCATION);
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+			List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+			resultList.addAll(persistedList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultList;
 	}
 	
-	public void saveToPersistence() {
-		
+	public void saveToPersistence(List<ValidAccount> validAccounts) {
+		try {
+			File file = new File(VALID_ACCOUNTS_LOCATION);
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			objectOutputStream.writeObject(validAccounts);
+			objectOutputStream.close();
+			fileOutputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
