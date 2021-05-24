0378470195795f7ecfd9c286910105f9f4bac617
kongaman <christian.kranz@gmail.com>
2021-05-24 14:37:30 +0200

    passwords are encrypted before saving

--- src/com/ck/controller/persistence/PersistenceAccess.java
+++ src/com/ck/controller/persistence/PersistenceAccess.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class PersistenceAccess {
 	
 	private String VALID_ACCOUNTS_LOCATION = System.getProperty("user.home") + File.separator + "validAccount.ser";
+	private Encoder encoder = new Encoder();
 
 	public List<ValidAccount> loadFromPersistence() {
 		List<ValidAccount> resultList = new ArrayList<>();
@@ -18,6 +19,7 @@ public class PersistenceAccess {
 			FileInputStream fileInputStream = new FileInputStream(VALID_ACCOUNTS_LOCATION);
 			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 			List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+			decodePasswords(persistedList);
 			resultList.addAll(persistedList);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -25,11 +27,28 @@ public class PersistenceAccess {
 		return resultList;
 	}
 	
+	private void decodePasswords(List<ValidAccount> persistedList) {
+		for (ValidAccount validAccount : persistedList) {
+			String originalPasswords = validAccount.getPassword();
+			validAccount.setPassword(encoder.decode(originalPasswords));
+		}
+		
+	}
+	
+	private void encodePasswords(List<ValidAccount> persistedList) {
+		for (ValidAccount validAccount : persistedList) {
+			String originalPasswords = validAccount.getPassword();
+			validAccount.setPassword(encoder.encode(originalPasswords));
+		}
+		
+	}
+
 	public void saveToPersistence(List<ValidAccount> validAccounts) {
 		try {
 			File file = new File(VALID_ACCOUNTS_LOCATION);
 			FileOutputStream fileOutputStream = new FileOutputStream(file);
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			encodePasswords(validAccounts);
 			objectOutputStream.writeObject(validAccounts);
 			objectOutputStream.close();
 			fileOutputStream.close();
